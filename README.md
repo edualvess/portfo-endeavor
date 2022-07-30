d1ecb18898656fdc9610337763080a265d9b5001
edualvess <alves.edu@outlook.com>
2022-07-30 10:04:01 -0300

    Added excepction handling to submission function

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # portfo-endeavor
 This repository will keep track of our tireless endeavors to master a full-stack experience as a Portfolio web-page
+I'll be gradually improving the page and incresing its functionality, as well as bringing out my projects as I finish them.
--- server.py
+++ server.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template, request, redirect
 import csv
+
 app = Flask(__name__)
-#print(__name__)
+
+
+# home page router function
 @app.route('/')
 def root():
     return render_template('index.html')
 
+
+# page router function
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
 
-def write_file(data):
-    with open('database.txt', mode='a') as database:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        database.write(f'\n{email},{subject},{message}')
 
+# csv file writing for persistent form data
 def write_csv_file(data):
     with open('database.csv', mode='a', newline='') as database2:
         email = data['email']
@@ -25,12 +25,18 @@ def write_csv_file(data):
         csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email, subject, message])
 
+
+# app collecting form data and submitting to file
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_csv_file(data)
+        try:
+            data = request.form.to_dict()
+            write_csv_file(data)
+        except:
+            return 'Failed to save submitted form, please try again'
+
         return redirect('/thankyou.html')
 
     else:
-        return "Something went wrong"
+        return "Unable to submit form data, please try again later"
